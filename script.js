49273da3c097a2f173c5361b35f3782db28c1245
Maks <makso544g@gmail.com>
2024-05-28 18:05:03 +0100

    Menu Responsiveness optimization

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
-document.addEventListener("DOMContentLoaded", function() {
-  const basketButton = document.getElementById('basketButton');
+document.addEventListener('DOMContentLoaded', () => {
+  const buttons = document.querySelectorAll('.icon-button');
 
-  basketButton.addEventListener('click', function() {
-    const icon = this.querySelector('i');
-    
-    if (icon.classList.contains('fa-shopping-basket')) {
-      icon.classList.remove('fa-shopping-basket');
-      icon.classList.add('fa-check');
-    } else {
-      icon.classList.remove('fa-check');
-      icon.classList.add('fa-shopping-basket');
-    }
+  buttons.forEach(button => {
+    button.addEventListener('click', () => {
+      const icon = button.querySelector('i');
+      if (icon.classList.contains('fa-shopping-basket')) {
+        icon.classList.remove('fa-shopping-basket');
+        icon.classList.add('fa-check');
+      } else {
+        icon.classList.remove('fa-check');
+        icon.classList.add('fa-shopping-basket');
+      }
+    });
   });
 });
