b2a0a8b9e5c7111387344d0aeed9e3319c047d49
Maks <makso544g@gmail.com>
2024-05-29 00:15:47 +0100

    Added a menu fade in effect on scroll

--- script.js
+++ script.js
@@ -1,16 +1,41 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const buttons = document.querySelectorAll('.icon-button');
+document.addEventListener("DOMContentLoaded", () => {
+  const buttons = document.querySelectorAll(".icon-button");
 
-  buttons.forEach(button => {
-    button.addEventListener('click', () => {
-      const icon = button.querySelector('i');
-      if (icon.classList.contains('fa-shopping-basket')) {
-        icon.classList.remove('fa-shopping-basket');
-        icon.classList.add('fa-check');
+  buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+      const icon = button.querySelector("i");
+      if (icon.classList.contains("fa-shopping-basket")) {
+        icon.classList.remove("fa-shopping-basket");
+        icon.classList.add("fa-check");
       } else {
-        icon.classList.remove('fa-check');
-        icon.classList.add('fa-shopping-basket');
+        icon.classList.remove("fa-check");
+        icon.classList.add("fa-shopping-basket");
       }
     });
   });
+
+  const seeMenuButton = document.getElementById("seeMenuButton");
+  const menuSection = document.getElementById("menuSection");
+
+  seeMenuButton.addEventListener("click", function () {
+    menuSection.scrollIntoView({ behavior: "smooth" });
+  });
+
+  const elementsToFadeIn = document.querySelectorAll('.hidden');
+
+  const fadeInOnScroll = () => {
+    elementsToFadeIn.forEach(element => {
+      const elementPosition = element.getBoundingClientRect().top;
+      const viewportHeight = window.innerHeight;
+
+      if (elementPosition < viewportHeight - 50) {
+        element.classList.add('fade-in');
+      }
+    });
+  };
+
+  window.addEventListener('scroll', fadeInOnScroll);
+  // Trigger fade-in on page load
+  fadeInOnScroll();
+
 });
