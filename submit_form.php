6514def09c27b2a9042685fcdfc5e7d19c273f7f
Maks <makso544g@gmail.com>
2024-06-06 15:59:41 +0100

    Added some Raw php to the contact form and a video iframe to the main page

---
+++ submit_form.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Forno Felice</title>
+</head>
+<body>
+
+Thank you for your contacting us <?php echo $_POST["name"]; ?><br>
+Your email address is: <?php echo $_POST["email"]; ?><br>
+Subject: <?php echo $_POST["subject"]; ?><br>
+Message: <?php echo $_POST["message"]; ?>
+
+
+  
+</body>
+</html>
